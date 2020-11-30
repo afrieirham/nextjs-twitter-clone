0a4829d84adf5badc0f3a66cec5f6e69b5f0ff22
afrie <itsafrie@gmail.com>
2020-12-01 01:29:06 +0800

    Add faker for data generation

--- data.js
+++ data.js
@@ -1,90 +1,19 @@
-export default [
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-  {
-    id: Math.random(),
-    tweet: 'hello',
-  },
-]
+import faker from 'faker'
+
+const createTweet = (total) => {
+  let array = []
+  for (let i = 0; i < total; i++) {
+    array.push({
+      id: faker.random.uuid(),
+      tweet: faker.lorem.sentence(),
+      username: faker.internet.userName(),
+      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      avater: faker.random.image(),
+      text: faker.lorem.paragraph(),
+    })
+  }
+
+  return array
+}
+
+export { createTweet }
