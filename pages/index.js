56f1a5b25a7f7809c61aac35c7329b4aea2b2a4b
afrie <itsafrie@gmail.com>
2020-12-01 01:32:10 +0800

    Implement tweet component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
-import { Box } from '@chakra-ui/react'
-import data from '../data'
+import { Avatar, Box, chakra, Flex } from '@chakra-ui/react'
+import { createTweet } from '../data'
 
 function Home() {
   return (
     <>
-      {data.map((i) => (
-        <Box key={i.id} p='5' borderBottom='1px' borderBottomColor='gray.100'>
-          {i.tweet}
-        </Box>
+      {createTweet(90).map((i) => (
+        <Flex key={i.id} p='5' borderBottom='1px' borderBottomColor='gray.100'>
+          <Box mr='5'>
+            <Avatar src={i.avater} />
+          </Box>
+          <Flex flexDirection='column'>
+            <Box>
+              <chakra.span fontSize='sm'>
+                <Flex>
+                  <chakra.span isTruncated noOfLines='1' mr='1'>
+                    <chakra.span fontWeight='bold'>{i.name}</chakra.span>{' '}
+                    <chakra.span color='gray.500'>@{i.username}</chakra.span>
+                  </chakra.span>
+                  <chakra.span flexShrink='0' color='gray.500'>
+                    • 58m
+                  </chakra.span>
+                </Flex>
+              </chakra.span>
+            </Box>
+            <Box>{i.tweet}</Box>
+          </Flex>
+        </Flex>
       ))}
     </>
   )
