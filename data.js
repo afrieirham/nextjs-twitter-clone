23d6b4fc30a6ee968fa5d35777a1fdac4b87d751
afrie <itsafrie@gmail.com>
2020-12-01 20:46:48 +0800

    Use firstName and lastName for username

--- data.js
+++ data.js
@@ -3,11 +3,13 @@ import faker from 'faker'
 const createTweet = (total) => {
   let array = []
   for (let i = 0; i < total; i++) {
+    const firstName = faker.name.firstName()
+    const lastName = faker.name.lastName()
     array.push({
       id: faker.random.uuid(),
       tweet: faker.lorem.sentence(),
-      username: faker.internet.userName(),
-      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      username: faker.internet.userName(firstName, lastName),
+      name: `${firstName} ${lastName}`,
       avater: faker.random.image(),
       text: faker.lorem.paragraph(),
     })
